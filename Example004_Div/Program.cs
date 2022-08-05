bc2a472b572e2de913c1cb43d2b9130d61828361
DenisKap <denyarcity@yandex.ru>
2022-08-05 12:32:54 +0300

    Example 003 random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 22;
+double numberB = 7;
+Console.WriteLine(numberA / numberB);
+
