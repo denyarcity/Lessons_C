ce05905dc850e896376c4bb9e373fcfc8852d296
DenisKap <denyarcity@yandex.ru>
2022-08-17 14:55:22 +0300

    HW 8, 9, 10

---
+++ HW_C/Task_10/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// * В большинстве задач методов будет несколько
+
+Console.Write("Введите число и получите таблицу кубов чисел от 1 до N: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
